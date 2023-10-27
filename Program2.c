97e5ec38258d9ad90700c97ec8c0f6d95d477c63
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-10-27 19:52:29 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,21 @@
+// Accept two number form user and perform addition
+#include<stdio.h>
+
+int main()
+{
+    int i,j,k;
+
+    printf("Enter first number:\n");
+    scanf("%d",&i);
+
+    printf("Enter Second number:\n");
+    scanf("%d",&j);
+
+    k = i + j;
+
+    printf("Addition of number: %d\n",k);
+
+    return 0;
+
+
+}
