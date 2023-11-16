3022aca6b428a01d3409da06e4ed2ec4de79e1f0
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-11-16 21:10:48 +0530

    Add files via upload

---
+++ Program27.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void display()
+{
+    int iCnt =0; // Counter = iCnt
+    int iNo =5;
+    for(iCnt =1;iCnt <=iNo;iCnt ++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+int main()
+{
+    display();
+
+    return 0;
+}
---
+++ Program28.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void display()
+{
+    int iCnt =0; // Counter = iCnt
+    int iNo =5;
+    for(iCnt =0;iCnt <iNo;iCnt ++) // iCnt=0
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+int main()
+{
+    display();
+
+    return 0;
+}
+// array start by 0 and loop start by 1
