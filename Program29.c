941433d52df252996af23bab45699f952dcbf060
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-11-17 22:20:16 +0530

    Add files via upload

---
+++ Program29.c
@@ -0,0 +1,18 @@
+//Static
+#include<stdio.h>
+
+void display()
+{
+    int iCnt =0; // Counter = iCnt
+    int iNo =4;
+    for(iCnt =0;iCnt <=iNo;iCnt ++) // iCnt=0
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+int main()
+{
+    display();
+
+    return 0;
+}
---
+++ Program30.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void display()
+{
+    int iCnt =0; // Counter = iCnt
+    int iNo =5; // Hard Coded
+    for(iCnt =1;iCnt <=iNo;iCnt ++) // iCnt=0
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+int main()
+{
+    display();
+
+    return 0;
+}
