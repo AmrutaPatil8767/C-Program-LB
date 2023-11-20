edb26f9f2fdeaf70267db6b0e47e822b739732c8
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-11-20 21:10:26 +0530

    Add files via upload

---
+++ Program38.c
@@ -0,0 +1,19 @@
+//Display Number in Resverse on Screen
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt=0;
+    for(iCnt=1; iCnt<= iNo;iCnt++)
+    {
+        printf(" *\n",iCnt);
+    }
+}
+int main()
+{
+    int iValue=0;
+    printf("Enter Number to print Number \n");
+    scanf("%d",&iValue);
+    Display(iValue);
+    return 0;
+}
