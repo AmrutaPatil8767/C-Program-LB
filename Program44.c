bbd1c130e57c3c28d9477f6ef40f29289182a728
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-11-24 22:35:26 +0530

    Add files via upload

---
+++ Program44.c
@@ -0,0 +1,46 @@
+//Accept Input form user and check wheather perfect number or not (6,28,496)
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckNumber(int iNo)
+{
+    auto int iCnt=0;
+    auto int iSum=0;
+
+    for(iCnt =1; iCnt <= (iNo/2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iSum = iSum+iCnt;
+        }
+    }
+    if(iSum == iNo)
+    {
+                   return true;
+    }
+     else
+     {
+         return false;
+    }
+    }
+
+int main()
+{
+    auto int iValue=0;
+    auto bool bRet =false;
+
+    printf("Enter a number: \n");
+    scanf("%d",&iValue);
+
+    bRet=CheckNumber(iValue);
+    if(bRet == true)
+    {
+        printf("%d is Perfect Number",iValue);
+    }
+    else
+    {
+        printf("%d is Not Perfect Number",iValue);
+    }
+
+    return 0;
+}
