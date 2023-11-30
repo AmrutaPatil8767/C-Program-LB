3b8fa9b35d9560559d61276c20a04ac51bf069fa
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-11-30 21:59:16 +0530

    Add files via upload

---
+++ Program47.c
@@ -0,0 +1,45 @@
+//Accept Input form user and check wheather Prime number or not (7,11,23) using flag
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    auto int iCnt=0;
+    bool bflag = true;
+
+    if(iNo <0)  // Updater
+    {
+        iNo=-iNo;
+    }
+
+    for(iCnt =2; iCnt <= (iNo/2); iCnt++)// iCnt =2 because 1 has all prime number
+    {
+        if((iNo % iCnt) == 0)
+        {
+          bflag= false;
+          break;
+        }
+    }
+    return bflag;  // bflag=true
+ }
+
+int main()
+{
+    auto int iValue=0;
+    auto bool bRet =false;
+
+    printf("Enter a number: \n");
+    scanf("%d",&iValue);
+
+    bRet=CheckPrime(iValue);
+    if(bRet == true)
+    {
+        printf("%d is Prime Number",iValue);
+    }
+    else
+    {
+        printf("%d is Not Prime Number",iValue);
+    }
+
+    return 0;
+}
