7d1bf853debcb497f362c4276412e6a1e20c22e2
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-12-02 22:02:25 +0530

    Add files via upload

---
+++ Program49.c
@@ -0,0 +1,26 @@
+
+// Program to display digit
+#include<stdio.h>
+void Display(int iNo)
+{
+    int iDigit =0;
+
+    //jo paryanta iNo 0 hota nahi to paryanta fira
+    while (iNo !=0)
+    {
+        printf("---------------------------------------------------------\n");
+        iDigit = iNo %10;
+        printf("Digit is : %d \n",iDigit);
+        iNo = iNo/10;
+        printf("Number is : %d \n",iNo);
+    }
+}
+int main()
+{
+    int iValue=0;
+    printf("Enter Number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    return 0;
+}
