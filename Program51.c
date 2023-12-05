87ff52b824e9a4d2494e63f70b9af3cf9e6b09d7
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-12-05 09:38:10 +0530

    Add files via upload

---
+++ Program51.c
@@ -0,0 +1,28 @@
+// Accept Number form user and calculate how much digit is there.(eg: 76543=5)
+# include<Stdio.h>
+int CountDigit(int iNo)
+{
+    auto int iCnt =0;
+    auto int iDigit =0;
+
+    while(iNo !=0)
+    {
+        iDigit=iNo %10;
+        iNo = iNo/10;
+        iCnt ++;
+    }
+    return iCnt;
+}
+int main()
+{
+    auto int iValue=0;
+    auto int iRet=0;
+
+    printf("Enter Number:\n");
+    scanf("%d",&iValue);
+
+    iRet=CountDigit(iValue);
+    printf("Number of Digit are :%d \n",iRet);
+    return 0;
+
+}
