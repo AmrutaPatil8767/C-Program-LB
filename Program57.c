c48bbc7c2bc910ac39bbcd79ef3338f9c91f269e
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-12-11 11:30:07 +0530

    Add files via upload

---
+++ Program57.c
@@ -0,0 +1,38 @@
+//Accpet number form user and checkwheather its palindrome or not.
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPalindrome(int iNo)
+{
+    int iCopy=iNo; // Xerox kadhali
+    int iDigit=0;
+    int iRev=0;
+    while(iNo !=0)
+    {
+        iDigit=iNo%10;
+        iNo=iNo/10;
+        iRev=(iRev*10)+iDigit;
+    }
+    return(iRev==iCopy);
+}
+
+ int main()
+ {
+    int iValue=0;
+    bool bRet=false;
+
+    printf("Enter Number:\n");
+    scanf("%d",&iValue);
+
+    bRet=CheckPalindrome(iValue);
+
+    if(bRet== true)
+    {
+        printf("%d is a Palindrome Number",iValue);
+    }
+    else
+    {
+        printf("%d is not a Palindrome Number",iValue);
+    }
+    return 0;
+ }
