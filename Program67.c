588ccf2e527b03012b5e374e7ff948ad5637283b
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-12-17 21:12:31 +0530

    Add files via upload

---
+++ Program67.c
@@ -0,0 +1,46 @@
+//Accept n Number from user and Calculate its Additon and display it.
+#include<stdio.h>
+#include<stdlib.h>
+
+int Addition(int Arr[],int iSize)
+{
+    int iCnt=0;
+    int iSum=0;
+
+//        1           2       3
+    for(iCnt=0; iCnt < iSize; iCnt++)
+    {
+       iSum=iSum+Arr[iCnt];
+    }
+    return iSum;
+}
+int main()
+{
+    int iCount=0;
+    int iCnt=0;
+    int *ptr=NULL;
+    int iRet=0;
+    
+    printf("Enter Number of Elements thatyou wants to Enter: \n");
+    scanf("%d",&iCount);
+
+    ptr=(int*)malloc(iCount* sizeof(int));
+
+    printf("Dynamic memory gets allocated succesully for %d element \n",iCount); // to get limit to values enter
+
+    printf("Enter the %d Values : \n",iCount);
+//        1        2         3
+    for(iCnt=0;iCnt <iCount;iCnt++)
+    {                                                   //O(N)
+        printf("\n Enter the element no %d:",iCnt+1);
+        scanf("%d",&ptr[iCnt]);//4
+    }
+    iRet=Addition(ptr,iCount);//Display(100,4)
+
+    printf("Addition of all Elements are:%d \n",iRet);
+
+    free(ptr);//(free(100))
+    printf("Dynamic memory gets deallocated Succesfully....\n");
+
+    return 0;
+}
