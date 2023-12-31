20e8272dc2d1faa120c173c187a76e7dd735757f
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2023-12-31 14:04:27 +0530

    Add files via upload

---
+++ Program95.c
@@ -0,0 +1,34 @@
+//Write program to display non-liner pattern(eg:-   1 2 3 4 
+//                                                  1 2 3 4
+//                                                  1 2 3 4  )Dynamic Pattern.Its Nested loop and its timecomplexcitiy is O(N2)
+#include<stdio.h>
+#include<conio.h>
+void Display(int iRow, int iCol)
+{
+    int i =0;
+    int j=0;
+    for(i=1;i<= iRow;i++)//outer loop
+    {
+        
+        for(j=1;j<=iCol;j++)// inner loop
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+    
+
+}
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+
+    printf("Enter a rows Count:\n");
+    scanf("%d",&iValue1);
+    printf("Enter a Columns Count:\n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+    return 0;
+}
