5be4371c1a4a072e49de677e9cd045125ae9d477
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2024-01-07 20:48:27 +0530

    Add files via upload

---
+++ Program110.c
@@ -0,0 +1,15 @@
+// To Print ASCII value of Charater
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     char ch='\0';
+
+    printf("Enter the character : \n");
+    scanf("%c",&ch);
+
+    printf("ASCII values of the charater is : %d\n",ch);
+
+    return 0;
+}
