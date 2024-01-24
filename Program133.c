c1ab8c68285c97bcc5b69e53f58de30daa2769f3
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2024-01-24 19:41:34 +0530

    Add files via upload

---
+++ Program133.c
@@ -0,0 +1,31 @@
+// Display String by a Updated String.
+#include<stdio.h>
+#include<conio.h>
+
+void strlwrX(char Str[])
+{
+    int iCnt =0;
+     while(*Str != '\0')
+     {
+        if((*Str >= 'A')&& (*Str <='Z'))
+        {
+            *Str =*Str + 32;
+        }
+         Str++;
+     }
+}
+
+int main()
+{
+    char Arr[20];
+    char cValue = '\0';
+
+    printf("Enter the String : \n");
+    scanf("%[^'\n']s",Arr);
+
+    strlwrX(Arr);
+
+    printf("Updated String is : %s\n",Arr);
+
+    return 0;
+}
