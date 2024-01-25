3d3e200ac9f7eea103cc48264c6d5cb02f8336f8
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2024-01-25 21:45:21 +0530

    Add files via upload

---
+++ Program134.c
@@ -0,0 +1,31 @@
+// Display String by a Updated String.
+#include<stdio.h>
+#include<conio.h>
+
+void struprX(char Str[])
+{
+    int iCnt =0;
+     while(*Str != '\0')
+     {
+        if((*Str >= 'a')&& (*Str <='z'))
+        {
+            *Str =*Str - 32;
+        }
+         Str++;
+     }
+}
+
+int main()
+{
+    char Arr[20];
+    char cValue = '\0';
+
+    printf("Enter the String : \n");
+    scanf("%[^'\n']s",Arr);
+
+    struprX(Arr);
+
+    printf("Updated String is : %s\n",Arr);
+
+    return 0;
+}
