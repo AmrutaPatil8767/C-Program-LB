6c84a572f829699827404dcf3ff4026ef05a1e64
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2024-01-28 21:32:08 +0530

    Add files via upload

---
+++ Program154.c
@@ -0,0 +1,24 @@
+// Linear Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+//Structure Declaration
+struct node
+{
+    int data;  //4 bytes
+    struct node *next;  //8 bytes - self referececial structure
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int main()
+{
+    PNODE First = NULL;
+    //struct node *First = NULL; // pointer 8 bytes
+
+
+
+    return 0;
+}
