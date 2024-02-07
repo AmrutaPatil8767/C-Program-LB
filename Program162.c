f41f2893479e7da48f8d538380cdbed1a77f3719
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2024-02-07 22:34:15 +0530

    Add files via upload

---
+++ Program162.c
@@ -0,0 +1,52 @@
+//Doubly Linear Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+
+    int data;
+    struct node *next;
+    struct node *prev;  //#
+};
+typedef struct Node NODE;
+typedef struct Node * PNODE;
+typedef struct Node ** PPNODE;
+
+void InsertFirst(PPNODE Head, int no)
+{
+
+}
+void InsertLast(PPNODE Head, int no)
+{
+
+}
+void InsertAtPos(PPNODE Head, int no, int iPos)
+{
+
+}
+void DeleteFirst(PPNODE Head, int no)
+{
+
+}
+void DeleteLast(PPNODE Head, int no)
+{
+
+}
+void DeleteAtPos(PPNODE Head, int no , int iPos)
+{
+
+}
+void Display(PNODE Head)
+{
+
+}
+int Count(PNODE Head)
+{
+    return 0;
+}
+int main()
+{
+    PNODE First = NULL;
+    return 0;
+}
