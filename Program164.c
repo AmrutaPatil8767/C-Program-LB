c75f2d50e6492ad1f2fa9c0e0560c98f34ff6dfc
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2024-02-08 21:49:44 +0530

    Add files via upload

---
+++ Program164.c
@@ -0,0 +1,23 @@
+//Singly circular Linear Linked List:
+
+#include<stdio.h>
+#include<stdlib.h>
+// typedef use in struct node in Program165.c file
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node**  PPNODE;
+
+int main()
+{
+    PNODE First = NULL;
+    PNODE Last = NULL;
+
+
+    return 0;
+}
